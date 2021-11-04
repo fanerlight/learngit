fb1c64a6e7569ee60970c67cb620efab2835a7c1
fanerlight <972079854@qq.com>
2021-11-04 10:18:54 +0800

    copy a logistic regression base torch

---
+++ LR_torch.py
@@ -0,0 +1,43 @@
+import  torch
+import numpy
+device=torch.device('cuda:0')
+
+# prepare the data set
+x_train=torch.Tensor([[1.],[2.],[3.]])
+y_train=torch.Tensor([[2.],[4,],[6.]])
+x_train=x_train.to(device)
+y_train=y_train.to(device)
+
+# establish the node model
+class LinearModel(torch.nn.Linear):
+    def __init__(self):
+        super(torch.nn.Linear,self).__init__()
+        self.linear=torch.nn.Linear(1,1)
+
+    def forward(self,x):
+        y_pred=self.linear(x)
+        return y_pred
+
+unit=LinearModel()
+unit=unit.to(device)
+# criterion and optim
+criterion=torch.nn.MSELoss(True,True).to(device)
+optim=torch.optim.Adam(unit.parameters(),0.01)
+
+# training cycle
+for epoch in range(1000):
+    y_pred=unit(x_train)
+    loss=criterion(y_pred,y_train)
+    print(epoch,loss)
+    optim.zero_grad()
+    loss.backward()
+    optim.step()
+
+# output the results
+print('w:',unit.linear.weight.data)
+print('b:',unit.linear.bias.data)
+
+# prediction
+x_test=torch.Tensor([[4.]]).to(device)
+y_test=unit(x_test)
+print('y_test:',y_test.data)
